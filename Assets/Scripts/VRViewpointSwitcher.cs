7b5eb663eac031af80c6d94384cbdf035e6a5011
Galko360 <163739663+Galko360@users.noreply.github.com>
2025-12-30 18:36:34 +0200

    View points and rays

--- Assets/Scripts/VRViewpointSwitcher.cs
+++ Assets/Scripts/VRViewpointSwitcher.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
+/*using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class VRViewpointSwitcher : MonoBehaviour
 {
     [SerializeField] private Transform xrOrigin;
     [SerializeField] private Transform[] viewpoints;
+    [SerializeField] private InputAction input;
+
 
     private int index = 0;
 
@@ -14,6 +17,66 @@ public class VRViewpointSwitcher : MonoBehaviour
 
         index = (index + 1) % viewpoints.Length;
         xrOrigin.SetPositionAndRotation(
+            viewpoints[index].position,
+            viewpoints[index].rotation
+        );
+    }
+
+    public void SwitchView(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            NextView();
+            Debug.Log("pressed switch");
+        }
+    }
+}
+*/
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VRViewpointSwitcher : MonoBehaviour
+{
+    [Header("XR Origin Root")]
+    [SerializeField] private Transform xrOrigin;
+
+    [Header("Viewpoints")]
+    [SerializeField] private Transform[] viewpoints;
+
+    private InputSystem_Actions input;
+    private int index = 0;
+
+    private void Awake()
+    {
+        input = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        input.Player.SwitchView.performed += OnSwitchView;
+        input.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Player.SwitchView.performed -= OnSwitchView;
+        input.Player.Disable();
+    }
+
+    private void OnSwitchView(InputAction.CallbackContext ctx)
+    {
+        NextView();
+    }
+
+    public void NextView()
+    {
+        if (xrOrigin == null || viewpoints.Length == 0)
+            return;
+
+        index = (index + 1) % viewpoints.Length;
+        xrOrigin.SetPositionAndRotation
+            (
             viewpoints[index].position,
             viewpoints[index].rotation
         );
